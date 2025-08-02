10a6a90d350bfe690c509a43b26fe45f54c9b50f
ysk3a <53593899+ysk3a@users.noreply.github.com>
2025-08-02 01:40:48 -0400

    aggrid theme to match app theme

--- src/components/EmployeeTable.vue
+++ src/components/EmployeeTable.vue
@@ -1,11 +1,9 @@
 <template>
-    <div style="height: 100%">
-                <ag-grid-vue
-      style="width: 100%; height: 100%;"
-      @grid-ready="onGridReady"
-      :rowData="rowData"
+  <div style="height: 100%" :data-ag-theme-mode="mode">
+    <ag-grid-vue style="width: 100%; height: 100%;" 
+        @grid-ready="onGridReady" :rowData="rowData"
       :columnDefs="columnDefs"></ag-grid-vue>
-        </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -22,23 +20,28 @@ import {
   ModuleRegistry,
   ValidationModule,
 } from "ag-grid-community";
+import { useColorMode } from '@vueuse/core';
+
+
+const mode = useColorMode()
+
 const columnDefs = ref<ColDef[]>([
-    { field: "make", },
-    { field: "model" },
-    { field: "price" },
-    { field: "manufactured" }
+  { field: "make", },
+  { field: "model" },
+  { field: "price" },
+  { field: "manufactured" }
 ]);
 
 const rowData = ref([
-    { make: "Toyota", model: "Celica", price: 35000 },
-    { make: "Ford", model: "Mondeo", price: 32000 },
-    { make: "Porsche", model: "Boxster", price: 72000 },
+  { make: "Toyota", model: "Celica", price: 35000 },
+  { make: "Ford", model: "Mondeo", price: 32000 },
+  { make: "Porsche", model: "Boxster", price: 72000 },
 ]);
-    const gridApi = shallowRef<GridApi | null>(null);
+const gridApi = shallowRef<GridApi | null>(null);
 
-    const onGridReady = (params: GridReadyEvent) => {
-      gridApi.value = params.api;
-    };
+const onGridReady = (params: GridReadyEvent) => {
+  gridApi.value = params.api;
+};
 // expose the custom cell renderer for use within AG Grid
 // defineExpose({CellComponentRenderer})
 </script>
